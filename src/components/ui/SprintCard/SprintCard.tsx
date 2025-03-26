68f4ece9958e4d94ba38907a01916c2ac88e6953
JeronimoCortez <jeronimofcortez3@gmail.com>
2025-03-26 17:41:50 -0300

    Centrado de button add en lista sprint, añadi hover a los button de cardSprint, renderiza dinamico en cardsprint

--- src/components/ui/SprintCard/SprintCard.tsx
+++ src/components/ui/SprintCard/SprintCard.tsx
@@ -1,15 +1,21 @@
+import { FC } from "react";
+import { ISprint } from "../../../types/ISprint";
 import DeleteButton from "../DeleteButton/DeleteButton";
 import EditButton from "../EditButton/EditButton";
 import TaskEyeButton from "../TaskEyeButton/TaskEyeButton";
 import styles from "./SprintCard.module.css";
 
-const SprintCard = () => {
+type IPropsSprintCard = {
+  sprint: ISprint;
+};
+
+const SprintCard: FC<IPropsSprintCard> = (sprint) => {
   return (
     <div className={styles.sprintCard}>
-      <div className={styles.sprintCardHeader}>Sprint 1</div>
+      <div className={styles.sprintCardHeader}>{sprint.sprint.nombre}</div>
       <div className={styles.sprintCardInfo}>
-        <div>Inicio: </div>
-        <div>Fin: </div>
+        <div>Inicio: {sprint.sprint.inicio.toISOString()}</div>
+        <div>Fin: {sprint.sprint.fin.toISOString()} </div>
       </div>
       <div className={styles.sprintCardButtons}>
         <TaskEyeButton />
--- src/components/ui/SprintsAside/SprintsAside.tsx
+++ src/components/ui/SprintsAside/SprintsAside.tsx
@@ -1,7 +1,14 @@
+import { ISprint } from "../../../types/ISprint";
 import SprintCard from "../SprintCard/SprintCard";
 import styles from "./SprintsAside.module.css";
 
 const SprintsAside = () => {
+  const sprint: ISprint = {
+    nombre: "Sprint 1",
+    inicio: new Date("2025-03-26"),
+    fin: new Date("2025-03-31"),
+  };
+
   return (
     <div className={styles.containerAside}>
       <button className={styles.buttonBacklog}>
@@ -11,7 +18,7 @@ const SprintsAside = () => {
         <h3 className={styles.title}>
           Lista de sprints
           <button className={styles.buttonAddSprint}>
-            <img src="./add.svg" alt="" />
+            <img className={styles.addIcon} src="./add.svg" alt="" />
           </button>
         </h3>
       </div>
@@ -19,7 +26,7 @@ const SprintsAside = () => {
       <div className={styles.line}></div>
 
       <div className={styles.sprintCardContainer}>
-        <SprintCard />
+        <SprintCard sprint={sprint} />
       </div>
     </div>
   );
