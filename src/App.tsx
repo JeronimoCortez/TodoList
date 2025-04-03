db194569d364315e3210e0ae147f3412e064eea5
JeronimoCortez <jeronimofcortez3@gmail.com>
2025-04-03 10:57:39 -0300

    Arreglo de errores

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-<<<<<<< Updated upstream
-import BacklogScreen from "./components/screens/BacklogScreen/BacklogScreen";
-function App() {
-  return (
-    <>
-      <BacklogScreen />
-=======
-import SprintScreen from "./components/screens/SprintScreen/SprintScreen";
 import { CreateSprintModal } from "./components/ui/CreateSprintModal/CreateSprintModal";
 function App() {
   return (
     <>
-      <SprintScreen />
       <CreateSprintModal />
->>>>>>> Stashed changes
     </>
   );
 }
