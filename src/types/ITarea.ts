8f1f1ab54c4399ef4141e638a0cc2d2bfa826d88
JeronimoCortez <jeronimofcortez3@gmail.com>
2025-04-09 20:28:40 -0300

    Controlador Backlog, controlador Sprint(todoListController)

--- src/types/ITarea.ts
+++ src/types/ITarea.ts
@@ -1,5 +1,9 @@
+import { estadosTareas } from "../enum/estadosTareas";
+
 export interface ITarea {
+  id: string;
   titulo: string;
   descripcion: string;
   fechaLimite: Date;
+  estado: estadosTareas;
 }
--- src/utils/constantes.ts
+++ src/utils/constantes.ts
@@ -1,1 +1,1 @@
-export const API_URL = "http://localhost:3000/todoListApp";
+export const API_URL = "http://localhost:3000";
